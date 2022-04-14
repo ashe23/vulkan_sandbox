4192350cf42cccc3d864c4354a1a8f7c923e1a4f
Ashot Barkhudaryan <ashot7410@gmail.com>
2022-04-14 13:36:19 +0400

    feat: add git submodule
    - added glfw lib as submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
 #include "vulkan/vulkan.h"
+#include <GLFW/glfw3.h>
 
 int main()
 {
-    std::cout << "AAA\n";
+    GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+	{
+		std::cerr << "Failed to initialize glfw\n";
+		return -1;
+	}
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		std::cerr << "Failed to create window\n";
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
     return 0;
 }
